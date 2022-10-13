67d0171d608a211f7602f011f7f2f8d0f0e4f590
Sergey Morgunov <hostage@me.com>
2022-10-13 12:36:34 +0300

    Handlebars layouts and docs example for preview

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,43 @@
 const bundleName = 'ui-bundle'
 
+const { basename } = require('path')
 const ZipPlugin = require('zip-webpack-plugin');
 const FaviconsPlugin = require('favicons-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const HtmlPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'production',
   plugins: [
     new ZipPlugin({ filename: bundleName }),
-    new FaviconsPlugin('logo.svg')
+    new HtmlPlugin({
+      publicPath: 'uiRootPath',
+      templateContent: ({htmlWebpackPlugin}) => `${faviconsTags(htmlWebpackPlugin)}`,
+      filename: 'partials/favicons.hbs',
+      inject: false
+    }),
+    new FaviconsPlugin({
+      logo: 'logo.svg',
+      favicons: { appName: 'Taymyr' },
+      inject: htmlPlugin => basename(htmlPlugin.options.filename).endsWith('favicons.hbs')
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'src/layouts', to: 'layouts' },
+        { from: 'src/partials', to: 'partials' },
+      ]
+    }),
   ],
   performance: {
     assetFilter: (filename) => !filename.startsWith(bundleName)
   }
 };
+
+function faviconsTags(htmlWebpackPlugin) {
+  return htmlWebpackPlugin.tags.headTags
+    // Skip script tag with bundle that generated HtmlPlugin by default
+    .filter((tag) => tag.tagName !== 'script')
+    .join('')
+    // It must be Handlebars template with root path as variable
+    .replaceAll('uiRootPath', '{{{uiRootPath}}}')
+}
